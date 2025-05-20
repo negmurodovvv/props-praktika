1d141d65f99286b1cc15a0b04dfd55b9dfe81075
negmurodovvv <forbeautifulfuture1@gmail.com>
2025-05-20 17:10:53 +0500

    ozgardi

--- src/mock-data/index.js
+++ src/mock-data/index.js
@@ -1,5 +1,12 @@
+import igor from '../public/images/Eight.svg'
 import emily from '../public/images/First.svg'
+import amina from '../public/images/Fourth.svg'
+import takumi from '../public/images/Nine.svg'
 import arjun from '../public/images/Second.svg'
+import emma from '../public/images/Seventh.svg'
+import chen from '../public/images/Sixth.svg'
+import carlos from '../public/images/Third.svg'
+
 export const data = [
 	{
 		name: 'Emily Jhonson',
@@ -12,5 +19,44 @@ export const data = [
 		firstText: 'Product Manager',
 		secondText: 'Product Development',
 		imageSource: arjun,
+	},
+	{
+		name: 'Carlos Hernández',
+		firstText: 'UI Designer',
+		secondText: 'Design',
+		imageSource: carlos,
+	},
+
+	{
+		name: 'Amina Idris',
+		firstText: 'Marketing Specialist',
+		secondText: 'Marketing',
+		imageSource: amina,
+	},
+	{
+		name: 'Takumi Sato',
+		firstText: 'HR Specialist',
+		secondText: 'Human Resources',
+		imageSource: takumi,
+	},
+
+	{
+		name: 'Chen Wei',
+		firstText: 'Data Analyst',
+		secondText: 'Data Science',
+		imageSource: chen,
+	},
+
+	{
+		name: 'Emma Dubois',
+		firstText: 'Sales Manager',
+		secondText: 'Sales',
+		imageSource: emma,
+	},
+	{
+		name: 'Igor Sokolov',
+		firstText: 'Quality Assurance',
+		secondText: 'Quality Control',
+		imageSource: igor,
 	},
 ]
